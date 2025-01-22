5be3d6c13721b3e94eaa4a12af522624ebb99688
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-22 21:44:09 +0500

    Working..

--- Blogging-app/src/models/users.model.js
+++ Blogging-app/src/models/users.model.js
@@ -1,41 +1,47 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from ".";
-import { timeStamp } from "console";
-import { type } from "os";
 
 const createUserModel = (sequelize) => {
-    const User = sequelize.define( 
+  const User = sequelize.define(
     "User",
     {
-    id:{
-       type: DataTypes.INTEGER,
-       primaryKey: true,
-       autoIncrement:true, 
-    },
-    fullname:{
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fullname: {
         type: DataTypes.STRING,
         allowedNull: false,
-        validate:{
-            notEmpty: {msg: "Fullname can not be Empty!!"},
-        }
-    },
-    email:{
+        validate: {
+          notEmpty: { msg: "Fullname can not be Empty!!" },
+        },
+      },
+      email: {
         type: DataTypes.STRING,
         allowedNull: false,
         unique: true,
-        validate:{
-            isEmail: {msg : "Invalid Email format"},
-        }
-    },
-    password:{
-        type: DataTypes.STRING
-    }
+        validate: {
+          isEmail: { msg: "Invalid Email format" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowedNull: false,
+        validate: {
+          len: {
+            args: [6, 18],
+            msg: "Password must be between 6 and 18 characters",
+          },
+        },
+      },
     },
     {
-        timeStamp:true,
-        tableName: "users",
+      timeStamp: true,
+      tableName: "users",
     }
-    );
-}
+  );
+  return User;
+};
 
-export {createUserModel};
+export { createUserModel };
